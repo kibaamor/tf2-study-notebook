c56b88d7f74a9a5a113c43784040827eeb27c3d6
KibaAmor <kibaamor@gmail.com>
2020-11-02 18:28:05 +0800

    chapter_9

--- docker.sh
+++ docker.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # docker run --network=host --rm -it -v `pwd`:/tf tensorflow/tensorflow:2.3.1-jupyter
-docker run --network=host --rm -it -v `pwd`:/tf tensorflow/tensorflow:2.3.1-gpu-jupyter
+docker run --network=host --rm -it -v `pwd`:/tf -e TF_FORCE_GPU_ALLOW_GROWTH=true tensorflow/tensorflow:2.3.1-gpu-jupyter
 
